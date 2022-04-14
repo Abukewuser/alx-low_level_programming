2bf37b5d96d822d8711970a8ed616ebaf2864832
Abukewuser <101568649+Abukewuser@users.noreply.github.com>
2022-04-14 18:00:11 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,41 +1,29 @@
-alx-low_level_programming/0x02-functions_nested_loops/5-sign.c
-@Chikeiho
-Chikeiho 5 sign
-Latest commit 2ad576b on Mar 16
- History
- 1 contributor
-Executable File  31 lines (28 sloc)  430 Bytes
-   
 #include "main.h"
 
 /**
- * print_sign - Check Holberton
- * @n: An input number
- * Description: Tohis function prints the sign of the number
- * Return: 1 if number is positive, o if number is 0, or
- * -1 if number is negative
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *
+ *@n: the int to print
+ * Return: Always 0.
  */
 int print_sign(int n)
 {
-	int value;
-
-	if (n > 0)
-	{
-		value = 1;
-		_putchar('+');
-	}
-	else if (n == 0)
-	{
-		value = 0;
-		_putchar('0');
-	}
-	else
-	{
-		value = -1;
-		_putchar('-');
-	}
-
-	return (value);
+if (n > 0)
+{
+_putchar ('+');
+return (1);
 }
-© 2022 GitHub, Inc.
-Term
+
+else if (n == 0)
+{
+_putchar ('0');
+return (0);
+}
+
+else
+{
+_putchar ('-');
+return (-1);
+}
+
+}
